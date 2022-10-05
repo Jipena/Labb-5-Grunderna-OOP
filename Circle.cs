cd99096310f6a9ec50f34fa0b72859ff29347ed7
Jesper Andersson <Jesper_Forsa@hotmail.com>
2022-10-05 12:38:27 +0200

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labb_5___Grunderna_OOP
+{
+    public class Circle //Class
+    {
+        public float pi = 3.141f;
+        public int Radius = 0;
+
+        public Circle(int radius) //Constructor
+        {
+            Radius = radius;
+        }
+
+        public double getArea() //Metod
+        {
+            return Radius * Radius * pi;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Labb_5___Grunderna_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c1 = new Circle(5);
+            Circle c2 = new Circle(6);
+            Console.WriteLine(c1.getArea());
+            Console.WriteLine(c2.getArea());
+        }
+    }
+}
